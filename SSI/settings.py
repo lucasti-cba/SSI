e6085c06ac17915cb8659499cd4fd1bc5a8eb0cf
Lucas A. Batista <lucasti-cba@outlook.com>
2021-10-28 10:54:13 -0400

    Update settings.py

--- SSI/settings.py
+++ SSI/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'SSI',
         'USER': 'SSI',
-        'PASSWORD': 'B475csdu@#',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
